7f528d7f6c42376001fe6e72f9061cf632cac6b8
hoangk51 <phamhuyhoang1112@gmail.com>
2020-09-10 01:52:19 -0700

    init

---
+++ src/AdminLTETemplatesServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Hoangk51\AdminLTETemplates;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminLTETemplatesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../views', 'livewire-templates');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
